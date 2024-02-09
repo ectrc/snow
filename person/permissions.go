04fa8f53e15ea9acc931b282592f424a85dd6f3f
Eccentric <eccentric@retrac.site>
2024-02-09 23:56:06 +0000

    Add permissions test

--- person/permissions.go
+++ person/permissions.go
@@ -1,5 +1,7 @@
 package person
 
+import "github.com/ectrc/snow/aid"
+
 type Permission int64
 
 // DO NOT MOVE THE ORDER OF THESE PERMISSIONS AS THEY ARE USED IN THE DATABASE
@@ -14,5 +16,33 @@ const (
 	PermissionOwner
 	PermissionDonator
 
-	PermissionAll Permission = 1<<iota - 1
+	permissionRealAll = 1<<iota - 1
+	// every permission except owner
+	PermissionAll Permission = permissionRealAll ^ PermissionOwner
 )
+
+func (p Permission) StringifyMe() string {
+	aid.Print(p)
+	switch p {
+	case PermissionLookup:
+		return "Lookup"
+	case PermissionBan:
+		return "Ban"
+	case PermissionInformation:
+		return "Information"
+	case PermissionItemControl:
+		return "ItemControl"
+	case PermissionLockerControl:
+		return "LockerControl"
+	case PermissionPermissionControl:
+		return "PermissionControl"
+	case PermissionOwner:
+		return "Owner"
+	case PermissionDonator:
+		return "Donator"
+	case PermissionAll:
+		return "All"
+	default:
+		return "Unknown"
+	}
+}
